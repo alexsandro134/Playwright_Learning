49142f2e7f617aba0bb0a02404ee82bda890eb72
Alex Le <tl130491@gmail.com>
2025-06-09 22:20:43 +0700

    Adding pw exercise_01

---
+++ tests/TodoMVC/todoMVC.spec.ts
@@ -0,0 +1,50 @@
+import { test, expect, Page } from '@playwright/test';
+
+test.describe.configure({ mode: 'serial' });
+
+let page: Page;
+test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+});
+
+test.afterAll(async () => {
+    await page.close();
+});
+
+test('Add new todo item', async ({ }) => {
+    await page.goto('/todomvc/#');
+    await page.getByPlaceholder('What needs to be done?').fill('Buy groceries');
+    await page.keyboard.press('Enter');
+    await expect(page.getByText('Buy groceries')).toBeVisible();
+
+    await page.getByPlaceholder('What needs to be done?').fill('Walk the dog');
+    await page.keyboard.press('Enter');
+    await expect(page.getByText('Walk the dog')).toBeVisible();
+})
+
+test('Mark todo as complete', async ({ }) => {
+    await page.getByText('Buy groceries').locator('..').getByRole('checkbox').check();
+    await expect(page.getByText('Buy groceries').locator('..').getByRole('checkbox')).toBeChecked();
+});
+
+test('Filter todos', async ({ }) => {
+
+    await page.getByText('Active').click();
+    await expect(page.getByText('Walk the dog')).toBeVisible();
+    await expect(page.getByText('Buy groceries')).not.toBeVisible();
+
+    // Expects page to have a heading with the name of Installation.
+    await page.getByRole('link').getByText('Completed').click();
+    await expect(page.getByText('Walk the dog')).not.toBeVisible();
+    await expect(page.getByText('Buy groceries')).toBeVisible();
+
+    await page.getByRole('link').getByText('All').click();
+    await expect(page.getByText('Walk the dog')).toBeVisible();
+    await expect(page.getByText('Buy groceries')).toBeVisible();
+});
+
+test('Delete todo item', async ({ }) => {
+    let buyGroceries = page.getByText('Buy groceries');
+    await buyGroceries.getByRole('button', { name: 'Delete' }).click();
+    await expect(page.getByText('Buy groceries')).not.toBeVisible();
+});
