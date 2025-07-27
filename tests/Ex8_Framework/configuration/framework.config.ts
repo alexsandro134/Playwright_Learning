fb90541b343c73c85b201257f405b581f0e12410
Alex Le <alexsandro134@gmail.com>
2025-07-27 21:04:51 +0700

    Update log when get config from undefined env and undefine BASE_URL

--- tests/Ex8_Framework/configuration/framework.config.ts
+++ tests/Ex8_Framework/configuration/framework.config.ts
@@ -2,4 +2,11 @@ export const frameworkConfig = {
     retries: 3,
     timeout: 15000,
     authPath: './.auth',
+    users: {
+        standard_user: 'standard_user',
+        locked_out_user: 'locked_out_user',
+        problem_user: 'problem_user',
+        performance_glitch_user: 'performance_glitch_user',
+        password: 'secret_sauce'
+    }
 }
--- tests/Ex8_Framework/configuration/getConfig.ts
+++ tests/Ex8_Framework/configuration/getConfig.ts
@@ -4,7 +4,15 @@ import * as dotenv from 'dotenv'
 
 function getConfig(environment: string = 'local') {
     const envConfig = dotenv.config({ path: `${__dirname}/env/${environment}.env` })
-    return _.merge(frameworkConfig, envConfig.parsed)
+    if (envConfig.error) {
+        throw new Error(`Failed to load ${environment}.env: ${envConfig.error.message}`)
+    }
+    const config = _.merge(frameworkConfig, envConfig.parsed)
+
+    if (!config.BASE_URL) {
+        throw new Error(`BASE_URL not found in ${environment}.env`)
+    }
+    return config
 }
 
 export { getConfig }
